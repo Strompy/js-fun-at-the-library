a4eb29636ab61eabf327b2215e8ad629233dbf0a
Phillip Strom <phillip.strom@gmail.com>
2021-02-16 16:44:24 -0700

    Pass all tests

--- src/librarian.js
+++ src/librarian.js
@@ -11,6 +11,23 @@ class Librarian {
     return `Hello, ${patron}!`;
     }
   }
+
+  findBook(title) {
+    for (var genre in this.library.shelves) {
+      for (var i = 0; i < this.library.shelves[genre].length; i++) {
+        if (this.library.shelves[genre][i].title === title) {
+          this.library.shelves[genre].splice(i, 1);
+          return `Yes, we have ${title}`;
+        }
+      }
+    }
+    return `Sorry, we do not have ${title}`
+  }
+
+  calculateLateFee(daysLate) {
+    return Math.ceil(daysLate * .25)
+  }
+
 }
 
 module.exports = Librarian;
