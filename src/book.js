250210e95a01a1b7620dd0977f883f548c3cfa05
Phillip Strom <phillip.strom@gmail.com>
2021-02-16 15:34:12 -0700

    pass book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,43 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return 'The ' + title;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review)) {
+    reviews.push(review);
+  }
+}
+
+function calculatePageCount(book) {
+  return book.length * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title,
+    mainCharacter,
+    genre,
+    pageCount: calculatePageCount(title)
+  }
+}
+
+function editBook(book) {
+  book.pageCount *= 0.75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
