b73526f99a3fe17de1760aa394148d2c4c4dd904
Phillip Strom <phillip.strom@gmail.com>
2021-02-16 16:24:48 -0700

    Pass library tests

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,36 @@
+function createLibrary(name) {
+  return {
+    name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+}
+
+function addBook(library, book) {
+  library.shelves[book.genre].push(book);
+}
+
+function checkoutBook(library, title, genre) {
+  let success;
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === title) {
+      library.shelves[genre].splice(i, 1)
+      success = `You have now checked out ${title} from the ${library.name}`
+    }
+  }
+  if (success) {
+    return success;
+  } else {
+    return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+  }
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
