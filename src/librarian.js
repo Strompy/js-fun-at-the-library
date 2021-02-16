bd1617e084425139f5929e19892d0a71ed300d55
Phillip Strom <phillip.strom@gmail.com>
2021-02-16 16:28:35 -0700

    Create librarian and greet function

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,15 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+
+  greetPatron(patron, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${patron}!`;
+    } else {
+    return `Hello, ${patron}!`;
+    }
   }
 }
 
